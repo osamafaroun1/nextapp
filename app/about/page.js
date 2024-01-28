a8a8c077557a8269c673db898c832cce4484612f
osamafaroun1 <osamafaroun7@gmail.com>
2024-01-28 16:59:10 +0000

    main

---
+++ app/about/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/about.module.css'
+
+const page = () => {
+  return (
+    <div >
+       <h1 className={styles.custom}>osama about</h1>
+    </div>
+  )
+}
+
+export default page
---
+++ app/page.js
@@ -0,0 +1,9 @@
+import styles from './styles/page.module.css'
+
+export default function Home() {
+  return (
+    <main>
+       <h1 className={styles.custom_h1}>osama page</h1>
+    </main>
+  );
+}
